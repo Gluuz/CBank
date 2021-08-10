906e8ba63c892d41a074597003764d9dfc05bbf4
Gluuz <brunogluszczukk1@gmail.com>
2021-08-10 10:57:33 -0300

    Adicionando bonificaçao.

--- Funcionarios/diretor.js
+++ Funcionarios/diretor.js
@@ -1,7 +1,8 @@
-import { Funcionario } from "./funcionarios";
+import { Funcionario } from "./funcionarios.js";
 
 export class Diretor extends Funcionario{
     constructor(nome,salario,cpf){
         super(nome,salario,cpf)
+        this._bonificacao = 2;
     }
 }
